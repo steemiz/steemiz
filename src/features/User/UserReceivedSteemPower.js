032d452595f39156791f27c3b6d6698c6aec8a20
stoodkev <32375533+stoodkev@users.noreply.github.com>
2017-11-12 21:09:25 +0800

    Create UserReceivedSteemPower.js

---
+++ src/features/User/UserReceivedSteemPower.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import numeral from 'numeral';
+
+function UserReceivedSteemPower({ account }) {
+  return (
+    <span>
+      {numeral(account.steemPowerReceived).format('0,0.00')}
+    </span>
+  );
+}
+
+UserSteemPower.propTypes = {
+  account: PropTypes.object.isRequired,
+};
+
+export default UserSteemPower;
